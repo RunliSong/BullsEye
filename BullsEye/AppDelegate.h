53d1f2ce909c5256f87580fe008f720ecc549ee5
Runli Song <runlisong@10-140-15-87.wifi-m.deakin.edu.au>
2014-04-07 16:38:00 +1000

    Initial Commit

---
+++ BullsEye/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BullsEye
+//
+//  Created by Runli Song on 14-4-7.
+//  Copyright (c) 2014年 James. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BullsEye/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BullsEye
+//
+//  Created by Runli Song on 14-4-7.
+//  Copyright (c) 2014年 James. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
